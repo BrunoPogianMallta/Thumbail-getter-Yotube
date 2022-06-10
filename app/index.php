70c5846512f7b3e5f6f99f8c89c21cf9050a7980
BrunoPogianMallta <brunopmalltaads@gmail.com>
2022-06-10 17:03:18 -0300

    setup project

---
+++ app/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pegar Thumbnail - Youtube</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+
+<h2>Pegue Thumbnail de Qualquer Vídeo do Youtube</h2>
+
+<p>Para pegar a Thumbnail do vídeo do Youtube<br/>
+
+
+
+Basta inserir a URL, no formato abaixo:<br/>
+
+<b>http://www.youtube.com/watch?v=erLk59H86ww</b></p>
+
+<form method="post">
+
+<input type="text" name="url">
+
+<input type="submit" name="acao" value="Pegar Thumb!">
+
+</form>
+</body>
+</html>
